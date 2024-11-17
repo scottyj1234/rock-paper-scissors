c959453af6fc767892c5c6abe0123ed72d5a48f4
Scott J <70438931+scottyj1234@users.noreply.github.com.>
2024-11-17 12:21:17 -0600

    Added computer choice function

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,1 +1,14 @@
-console.log("Hello world!")
+function getComputerChoice() {
+  const randomChoiceInteger = Math.random() * 3
+  if(randomChoiceInteger < 1) {
+    return "rock"
+  } else if (randomChoiceInteger < 2) {
+    return "paper"
+  } else {
+    return "scissors"
+  }
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(getComputerChoice())
+}
